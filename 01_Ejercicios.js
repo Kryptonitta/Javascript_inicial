e2e112b756edb07d39b8d3a56b243af19f2cf77d
Krypto <karina.zwenger@live.com.ar>
2022-05-19 00:54:11 -0300

    ejercicios clase 1

---
+++ 01_Ejercicios.js
@@ -0,0 +1,11 @@
+//CLASE 1: Conceptos generales, sintaxis y variables
+
+//EJERCICIO 1: Crear tres (3) variables para asignar el nombre, apellido y edad de Homero, respectivamente. Es indistinto el empleo de var o  let en la declaración.
+
+//EJERCICIO 2: Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.
+
+//EJERCICIO 3: Declarar variables para representar la información de un carnet de conducir. Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.
+
+//EJERCICIO 4: Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. Luego, concatenar dichas entradas y efectuar una salida por alerta.
+
+//EJERCICIO 5: Solicitar al usuario uno o más precios. Luego, efectuar un descuento en porcentajes (20%, 30%, etcétera) sobre los valores ingresados, y realizar una salida.
